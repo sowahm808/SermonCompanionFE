629e1b0e7f3ea440bde198b68c66b0c03aa71ab1
sowahm808 <sowahm808@gmail.com>
2025-06-09 22:34:57 -0500

    Add Angular reflection page with separate template and styles

--- features/reflection/reflection.page.ts
+++ features/reflection/reflection.page.ts
@@ -1,2 +1,37 @@
-export class ReflectionPage extends HTMLElement {}
-customElements.define('reflection-page', ReflectionPage);
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { StorageService } from '../../core/services/storage.service';
+
+export interface Reflection {
+  id: number;
+  text: string;
+}
+
+@Component({
+  selector: 'app-reflection',
+  standalone: true,
+  imports: [IonicModule, FormsModule, CommonModule],
+  templateUrl: './reflection.page.html',
+  styleUrls: ['./reflection.page.css']
+})
+export class ReflectionPage {
+  text = '';
+  reflections: Reflection[] = [];
+
+  constructor(private storage: StorageService) {
+    this.load();
+  }
+
+  load() {
+    this.reflections = this.storage.get<Reflection[]>('reflections') || [];
+  }
+
+  save() {
+    const r: Reflection = { id: Date.now(), text: this.text };
+    this.reflections.push(r);
+    this.storage.set('reflections', this.reflections);
+    this.text = '';
+  }
+}
