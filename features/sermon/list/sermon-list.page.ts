96fce98d27f234dc8cedf945e01188ecb5ee5f09
sowahm808 <sowahm808@gmail.com>
2025-06-09 22:27:42 -0500

    Implement sermon features and external templates

--- features/sermon/list/sermon-list.page.ts
+++ features/sermon/list/sermon-list.page.ts
@@ -1,2 +1,25 @@
-export class SermonListPage extends HTMLElement {}
-customElements.define('sermon-list-page', SermonListPage);
+import { Component, EventEmitter, Output } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { StorageService } from '../../../core/services/storage.service';
+import { Sermon } from '../create/create-sermon.page';
+
+@Component({
+  selector: 'app-sermon-list',
+  standalone: true,
+  imports: [IonicModule, CommonModule],
+  templateUrl: './sermon-list.page.html',
+  styleUrls: ['./sermon-list.page.css']
+})
+export class SermonListPage {
+  sermons: Sermon[] = [];
+  @Output() select = new EventEmitter<Sermon>();
+
+  constructor(private storage: StorageService) {
+    this.load();
+  }
+
+  load() {
+    this.sermons = this.storage.get<Sermon[]>('sermons') || [];
+  }
+}
